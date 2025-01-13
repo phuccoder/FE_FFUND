6cc3e0e59faf01e474c4b22656d7f020b0ceef27
AnPT173 <phanthienan01072003@gmail.com>
2025-01-13 16:38:04 +0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    disableStaticImages: false,
+    domains: [],
+    remotePatterns: [],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      type: 'asset',
+    });
+    return config;
+  },
+};
+
+module.exports = nextConfig;
