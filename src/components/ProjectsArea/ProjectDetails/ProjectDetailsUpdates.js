266dd727e4cde4c9fcb78f72b143f844c1686549
Nguyễn Thế Việt <144643183+VietNT810@users.noreply.github.com>
2025-03-03 10:19:37 +0700

    reload template

---
+++ src/components/ProjectsArea/ProjectDetails/ProjectDetailsUpdates.js
@@ -0,0 +1,62 @@
+import { projectDetailsUpdates } from "@/data/projectsArea";
+import React from "react";
+import Image from "next/image";
+
+const { id, updates } = projectDetailsUpdates;
+
+const ProjectDetailsUpdate = ({ update = {} }) => {
+  const { title, info, text, text2, image, id } = update;
+
+  return (
+    <div className="project-details-updates">
+      <div className="project-details-updates-top">
+        <h3 className="title">{title}</h3>
+        <div className="info-updates d-block d-sm-flex justify-content-between align-items-center">
+          <div className="info">
+            <Image
+              src={`/assets/images/${info.image}`}
+              alt={info.name}
+              unoptimized
+              style={{ width: '100%', height: 'auto' }} 
+              />
+            <span>
+              by{" "}
+              <span>
+                {info.name}
+                <span> {info.date}</span>
+              </span>
+            </span>
+          </div>
+          <div className="update">
+            <span>#{id} Update</span>
+          </div>
+        </div>
+      </div>
+      <div className="project-details-updates-content">
+        <p>{text}</p>
+        <p className="text">{text2}</p>
+        <div className="project-updates-thumb mt-50">
+          <Image
+            src={`/assets/images/${image}`}
+            alt={title}
+            fill
+            style={{ objectFit: 'cover' }}
+            unoptimized
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ProjectDetailsUpdates = ({ getClassName }) => {
+  return (
+    <div className={getClassName(id)} id={id}>
+      {updates.map((update) => (
+        <ProjectDetailsUpdate update={update} key={update.id} />
+      ))}
+    </div>
+  );
+};
+
+export default ProjectDetailsUpdates;
---
+++ src/data/guideArea.js
@@ -0,0 +1,7 @@
+import bg from "../../public/assets/images/guide-bg.jpg";
+
+export const guideArea = {
+  bg,
+  tagline: "Get Our Complete",
+  title: "Crowdfunding Guide",
+};
