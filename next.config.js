8664fef01b4293bd6682d7970560b02fb9ae0603
Nguyễn Thế Việt <144643183+VietNT810@users.noreply.github.com>
2025-03-03 10:19:37 +0700

    reload template

---
+++ next.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true,
+    disableStaticImages: true  // Add this line
+  },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    
+    // Add this configuration for handling static assets
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg|webp)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            publicPath: '/_next/static/images',
+            outputPath: 'static/images',
+          },
+        },
+      ],
+    });
+    
+    return config;
+  },
+}
+
+module.exports = nextConfig
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js"
-  ],
-  darkMode: ["class", '[data-theme="dark"]'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
-  daisyui: {
-    themes: ["light", "dark"],
-  },
-
-}
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+      "./public/index.html",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
