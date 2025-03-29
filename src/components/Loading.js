b752c955e0e1a9cd1847ed43881cbfbd7f9c2221
AnPT173 <phanthienan01072003@gmail.com>
2025-03-30 01:43:26 +0700

    updated project details story, edit project page and added transaction menu item

---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center py-12">
+      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
+      <span className="ml-3 text-gray-600">Loading transactions...</span>
+    </div>
+  );
+}
